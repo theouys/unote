448231a0f28c8bf7e820b3fd871bc00184689d6e
Theo Uys <theouys@outlook.com>
2021-02-21 21:38:13 +0200

    Initial Upload

---
+++ README.txt
@@ -0,0 +1,32 @@
+UNote is a very basic text editor. The idea behind it is to be very fast.
+It is written in Python3. That means you need to have pYthon 3 installed on
+your system and tkinter.
+
+You need the following to be installed as a prerequisite:
+
+Linux (Debian base/Ubuntu)
+--------------------
+sudo apt install python3
+sudo apt install python3-tk
+
+Linux (Arch)
+--------------------
+sudo pacman install python3
+sudo pacman install python3-tk
+
+Redhat/Fedora (Arch)
+--------------------
+yum install python3
+yum install python3-tk
+
+
+Install:
+--------
+Unzip the zip file into the /opt folder
+This will create the following folder /opt/unote with the following files in it
+    unote.sh (make sure it is executable)
+    unote.py
+    unote (Copy to /usr/bin)
+    unote
+    unote.application
+
---
+++ unote.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import tkinter.scrolledtext as scrolledtext
+from tkinter import filedialog as fd
+
+filename = "Untitled"
+
+#New Clean text box
+def mnu_New():
+    global filename
+    txt.delete('1.0', END)
+    filename = "Untitled"
+    root.title("UNote -" + filename)
+
+#Open new file
+def mnu_Open():
+    global filename
+    txt.delete('1.0', END)
+    filename = fd.askopenfilename()
+    opentxt  = open(filename,'r').read()
+    txt.insert( INSERT , opentxt)
+    root.title("UNote -" + filename)
+
+def mnu_Save():
+    global filename
+    a = txt.get('1.0', END)
+    file = open(filename,'w')
+    file.write(a)
+    file.close
+    root.title("UNote -" + filename)    
+
+def mnu_SaveAs():
+    global filename
+    a = txt.get('1.0', END)
+    filename = fd.asksaveasfile()
+    filename.write(a)
+    root.title("UNote -" + filename)        
+
+def mnu_Close():
+    global filename
+    txt.delete('1.0', END)
+    filename = "Untitled"
+    root.title("UNote -" + filename)
+
+def donothing():
+    filewin = Toplevel(root)
+    button = Button(filewin, text="Do nothing")
+    button.pack()
+
+#Main Window
+root = Tk()
+root.geometry("800x600")
+root.title("UNote - Written by Theo Uys")
+
+#Menu
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label=" New  ", command=mnu_New)
+filemenu.add_command(label=" Open  ", command=mnu_Open)
+filemenu.add_command(label=" Save  ", command=mnu_Save)
+filemenu.add_command(label=" Save As  ", command=mnu_SaveAs)
+filemenu.add_command(label=" Close  ", command=mnu_Close)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+
+root.config(menu=menubar)
+
+
+txt = scrolledtext.ScrolledText(root,undo=True)
+txt.pack(expand=True, fill=BOTH)
+
+
+
+root.mainloop()
